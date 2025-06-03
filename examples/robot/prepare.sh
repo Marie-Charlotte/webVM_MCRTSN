32ed7234c823130a97207b034d4b68438fcc2ab3
cordier-TSN <Cordier.claude.christian@gmail.com>
2025-06-03 12:51:32 +0200

    Initial commit

---
+++ examples/robot/prepare.sh
@@ -0,0 +1,7 @@
+alias ll='ls -l --color=never'
+alias la='ls -la --color=never'
+export PYTHONPATH="/home/user/Workspace/robot/:/home/user/Workspace/robot/libraries/:/home/user/Workspace/robot/resources/"
+python3 --version
+robot --version
+vi --version
+clear
---
+++ src/lib/messages.js
@@ -0,0 +1,34 @@
+const color= "\x1b[1;35m";
+const underline= "\x1b[94;4m";
+const normal= "\x1b[0m";
+export const introMessage = [
+	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
+	"|                                                                             |",
+	"| WebVM is a virtual Linux environment running in the browser via WebAssembly |",
+	"|                                                                             |",
+	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
+	"",
+	""
+];
+export const errorMessage = [
+	color + "CheerpX could not start" + normal,
+	"",
+	"Check the DevTools console for more information",
+	"",
+	"CheerpX is expected to work with recent desktop versions of Chrome, Edge, Firefox and Safari",
+	"",
+	"Give it a try from a desktop version / another browser!",
+	"",
+	"CheerpX internal error message is:",
+	""
+];
+export const unexpectedErrorMessage = [
+	color + "WebVM encountered an unexpected error" + normal,
+	"",
+	"Check the DevTools console for further information",
+	"",
+	"Please consider reporting a bug!",
+	"",
+	"CheerpX internal error message is:",
+	""
+];
