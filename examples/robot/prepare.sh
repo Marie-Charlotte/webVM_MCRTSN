5f127d921ddce1f1331ed5ecdead9bee420be643
cordier-TSN <Cordier.claude.christian@gmail.com>
2025-07-29 14:21:21 +0200

    Update prepare.sh

--- examples/robot/prepare.sh
+++ examples/robot/prepare.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH="/home/user/Workspace/robot/:/home/user/Workspace/robot/libraries/:/home/user/Workspace/robot/resources/"
 robot --version > /dev/null 2&1
-vim -o /home/user/Workspace/robot/tests/suite.robot -c :term
+vim -c :S -O /home/user/Workspace/robot/tests/suite.robot -c :term
